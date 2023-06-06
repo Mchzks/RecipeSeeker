b4f2667c71ac729aafcfc12ce2b84088eff829db
Daniel <art.copor@yahoo.com>
2023-06-06 01:27:02 -0500

    first commit

---
+++ src/views/MealDetails.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="max-w-[800px] mx-auto p-8">
+    <h1 class="text-4xl font-bold mb-5 text-blue-500">{{ meal.strMeal }}</h1>
+    <img :src="meal.strMealThumb" :alt="meal.strMeal" class="max-w-[100%]">
+    <div class="grid grid-cols-1 sm:grid-cols-3 text-lg py-2">
+      <div>
+        <strong class="font-bold text-blue-500">Category:</strong> {{ meal.strCategory }}
+      </div>
+      <div>
+        <strong class="font-bold text-blue-500">Area:</strong> {{ meal.strArea }}
+      </div>
+      <div>
+        <strong class="font-bold text-blue-500">Tags:</strong> {{ meal.strTags }}
+      </div>
+    </div>
+
+    <div class="my-3">
+      {{ meal.strInstructions }}
+    </div>
+
+    <div class="grid grid-cols-1 sm:grid-cols-2">
+      <div>
+        <h2 class="text-2xl font-semibold mb-2 text-blue-500">Ingredients</h2>
+        <ul>
+          <template v-for="(el, ind) of new Array(20)">
+            <li v-if="meal[`strIngredient${ind + 1}`]">
+            {{ ind + 1 }}. {{ meal[`strIngredient${ind + 1}`] }}
+            </li>
+          </template>
+        </ul>
+      </div>
+      <div>
+        <h2 class="text-2xl font-semibold mb-2 text-blue-500">Measures</h2>
+        <ul>
+          <template v-for="(el, ind) of new Array(20)">
+            <li v-if="meal[`strMeasure${ind + 1}`]">
+            {{ ind + 1 }}. {{ meal[`strMeasure${ind + 1}`] }}
+            </li>
+          </template>
+        </ul>
+      </div>
+      <div class="mt-4">
+        <YouTubeButton :href="meal.strYoutube" />
+        &nbsp;
+        <a
+          :href="meal.strSource"
+          target="_blank"
+          class="px-3 py-2 rounded border-2 text-white border-blue-600 bg-blue-500 hover:bg-blue-600 transition-colors"
+        >
+          View Original Source
+        </a>
+      </div>
+    </div>
+
+
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue'
+import { useRoute } from 'vue-router';
+import axiosClient from '../axiosClient';
+import YouTubeButton from '../components/YouTubeButton.vue';
+
+const route = useRoute();
+const meal = ref({})
+
+onMounted(() => {
+  axiosClient.get(`lookup.php?i=${route.params.id}`)
+    .then(({ data }) => {
+      meal.value = data.meals[0] || {}
+    })
+})
+
+</script>
